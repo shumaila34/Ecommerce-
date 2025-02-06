296f8f9150aa890375b71fcca817cd1a20c72db9
Fazeela <fazeelacharan@gmail.com>
2025-02-06 12:10:56 +0500

    ordertypes added

--- ecommerce/src/app/(user)/orders/components/OrderStatus.tsx
+++ ecommerce/src/app/(user)/orders/components/OrderStatus.tsx
@@ -1,8 +1,11 @@
-const OrderStatus = ({ status }: { status: string }) => {
+import { FC } from "react";
+import { OrderStatusProps } from "./Orders.type";
+
+const OrderStatus: FC<OrderStatusProps> = ({ status }) => {
   const statusColors: Record<string, string> = {
     Delivered: "text-green-600",
     Pending: "text-orange-500",
-    Processing:"text-pink-400"
+    Processing: "text-pink-400",
   };
 
   return (
